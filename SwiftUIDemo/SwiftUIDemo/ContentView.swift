f64a1b3a6cab5ef72d6442cf37982e47262ca835
Tsering Lama <tsenyk@Tserings-MacBook-Pro.local>
2019-09-24 15:51:57 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Tsering Lama on 9/24/19.
+//  Copyright © 2019 Tsering Lama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
